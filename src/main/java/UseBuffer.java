11a925a9f872e892bb990c986a51a3b2348d393e
刘耀文 <liuliren.mm@gmail.com>
2022-07-12 22:57:04 +0800

    演示put()、get()、flip()和rewind()方法如何使用

--- src/main/java/UseBuffer.java
+++ src/main/java/UseBuffer.java
@@ -17,7 +17,7 @@ public class UseBuffer {
     static Logger logger = Logger.getLogger("UseBuffer");
 
     /**
-     * allocate()方法测试
+     * allocate()方法测试 --> 创建对应的实例对象
      *
      * @description 演示如何使用Buffer类的allocate()方法
      * @author LiuYaoWen
@@ -25,14 +25,106 @@ public class UseBuffer {
      */
     public static void allocateTest() {
         // 创建一个 intBuffer 实例对象
-        intBuffer = IntBuffer.allocate( 20);
-        logger.info("----------after allocate----------");
+        intBuffer = IntBuffer.allocate(20);
+        commonLog(intBuffer, "----------after allocate----------");
+    }
+
+    /**
+     * put()方法测试 --> 将数据写入缓冲区中
+     *
+     * @author LiuYaoWen
+     * @date 2022/7/12 22:11
+     */
+    public static void putTest() {
+        // 创建一个 intBuffer 实例对象
+        intBuffer = IntBuffer.allocate(20);
+        int loop = 5;
+        for (int i = 0; i < loop; i++) {
+            // 将i写入缓冲区中
+            intBuffer.put(i);
+        }
+        commonLog(intBuffer, "---------after putTest------------");
+    }
+
+    /**
+     * flip()方法测试 --> 将缓冲区的写模式切换为读模式
+     *
+     * @author LiuYaoWen
+     * @date 2022/7/12 22:17
+     */
+    public static void flipTest() {
+        // 创建一个 intBuffer 实例对象
+        intBuffer = IntBuffer.allocate(20);
+        int loop = 5;
+        for (int i = 0; i < loop; i++) {
+            // 将i写入缓冲区中
+            intBuffer.put(i);
+        }
+        intBuffer.flip();
+        commonLog(intBuffer, "----------after flip-----------");
+    }
+
+    /**
+     * get()方法测试 --> 从缓冲区读取数据
+     *
+     * @author LiuYaoWen
+     * @date 2022/7/12 22:37
+     */
+    public static void getTest() {
+        intBuffer = IntBuffer.allocate(20);
+        int loop = 5;
+        for (int i = 0; i < loop; i++) {
+            intBuffer.put(i);
+        }
+        intBuffer.flip();
+        // 先读取两个数
+        loop = 2;
+        for (int i = 0; i < loop; i++) {
+            int j = intBuffer.get();
+            logger.info("Buffer data: " + j);
+        }
+        commonLog(intBuffer, "------after get 2 int-----------");
+        // 再读取三个数
+        loop = 3;
+        for (int i = 0; i < loop; i++) {
+            int j = intBuffer.get();
+            logger.info("Buffer data: " + j);
+        }
+        commonLog(intBuffer, "------after get 3 int-----------");
+    }
+
+    /**
+     * 通用的日志输出
+     *
+     * @param intBuffer intBuffer对象
+     * @param msg       日志信息
+     * @author LiuYaoWen
+     * @date 2022/7/12 22:33
+     */
+    public static void commonLog(IntBuffer intBuffer, String msg) {
+        logger.info(msg);
         logger.info("position=" + intBuffer.position());
         logger.info("limit=" + intBuffer.limit());
         logger.info("capacity=" + intBuffer.capacity());
     }
 
+    /**
+     * rewind()方法测试 --> 重置position值，使得Buffer可以重新从头开始读取
+     *
+     * @author LiuYaoWen
+     * @date 2022/7/12 22:48
+     */
+    public static void rewindTest() {
+        getTest();
+        intBuffer.rewind();
+        commonLog(intBuffer, "------------after rewind-----------");
+    }
+
     public static void main(String[] args) {
-        allocateTest();
+//        allocateTest();
+//        putTest();
+//        flipTest();
+//        getTest();
+        rewindTest();
     }
 }
